37d24cebc1bc09e9c11ac0d1411b9d24a7b6cbe6
ipandage <546657868@qq.com>
2015-11-24 18:01:00 +0800

    ios proxy

---
+++ src/main/java/net/tatabang/push/UmengProxyIos.java
@@ -0,0 +1,80 @@
+package net.tatabang.push;
+
+import net.tatabang.push.android.*;
+import net.tatabang.push.ios.*;
+import net.tatabang.push.util.UMConfigUtil;
+
+/**
+ * 友盟消息接口实现
+ * 
+ * @author xingang
+ * @date 2015年11月21日
+ */
+public class UmengProxyIos {
+
+    private final String appkey;
+    private final String appMasterSecret;
+
+    private PushClient client = new PushClient();
+
+	public UmengProxyIos() {
+		this.appkey = UMConfigUtil.getValue("ios_app_key");
+		this.appMasterSecret = UMConfigUtil.getValue("ios_master_secret");
+	}
+
+	public UmengProxyIos(String appkey, String masterSecret) {
+		this.appkey = appkey;
+		this.appMasterSecret = masterSecret;
+	}
+
+    /**
+     * 广播（Ios）
+     * @param broadcast
+     * @throws Exception
+     */
+    public void sendIOSBroadcast(IOSBroadcast broadcast) throws Exception {
+        broadcast.preSetParams(appkey, appMasterSecret);
+        client.send(broadcast);
+    }
+
+    /**
+     * 单播（Ios）
+     * @param unicast
+     * @throws Exception
+     */
+    public void sendIOSUnicast(IOSUnicast unicast) throws Exception {
+        unicast.preSetParams(appkey, appMasterSecret);
+        client.send(unicast);
+    }
+
+    /**
+     * 组播（Ios）
+     * @param groupcast
+     * @throws Exception
+     */
+    public void sendIOSGroupcast(IOSGroupcast groupcast) throws Exception {
+        groupcast.preSetParams(appkey, appMasterSecret);
+        client.send(groupcast);
+    }
+
+    /**
+     *
+     * @param customizedcast
+     * @throws Exception
+     */
+    public void sendIOSCustomizedcast(IOSCustomizedcast customizedcast) throws Exception {
+        customizedcast.preSetParams(appkey, appMasterSecret);
+        client.send(customizedcast);
+    }
+
+    /**
+     * 文件播 （Ios）
+     * @param filecast
+     * @throws Exception
+     */
+    public void sendIOSFilecast(IOSFilecast filecast) throws Exception {
+        filecast.preSetParams(appkey, appMasterSecret);
+        client.send(filecast);
+    }
+
+}
